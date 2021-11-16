7b87ec92e2dd7595c9985531b3a807e687e80f8b
pete <boris.the.alien@gmail.com>
2021-11-16 16:44:25 +0000

    reset drawdemo

--- drawdemo.c
+++ drawdemo.c
@@ -17,25 +17,6 @@ int main (int argc, char *argv[]){
             setpixel(0, x, y,colour);
         }
     }
-    flushscreen(14);
-    flushscreen(15);
-    flushscreen(16);
-    flushscreen(17);
-    flushscreen(18);
-    for (int y = 0; y < 200; y++)
-    {
-        for (int x = 0; x < 320; x++)
-        { 
-            setpixel(0, x, y,19);
-        }
-    }
-    for (int y = 0; y < 200; y++)
-    {
-        for (int x = 0; x < 320; x++)
-        { 
-            setpixel(0, x, y,12);
-        }
-    }
     getch();
     setvideomode(0x03);
     exit();
