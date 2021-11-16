bcb43f9f9661728dcb9d644744692e7d667dc62a
pete <boris.the.alien@gmail.com>
2021-11-16 21:00:22 +0000

    square tunnel draw

--- drawdemo.c
+++ drawdemo.c
@@ -12,10 +12,15 @@ int main(int argc, char *argv[])
     setvideomode(0x13);
 
     moveto(0, 100, 50);
-    lineto(0, 200, 50, 15);
-    lineto(0, 200, 150, 15);
-    lineto(0, 100, 150, 15);
-    lineto(0, 100, 50, 15);
+    for (int i = 0; i < 100; i += 3)
+    {
+        moveto(0, 25 + i, 10 + i);
+        lineto(0, 125 + i, 10 + i, 15);
+        lineto(0, 125 + i, 110 + i, 15);
+        lineto(0, 25 + i, 110 + i, 15);
+        lineto(0, 25 + i, 10 + i, 15);
+    }
+
     getch();
     setvideomode(0x03);
     exit();
